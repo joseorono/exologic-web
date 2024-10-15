429d0d9632e108be1a45ae14c5c2e23652bf2924
José Miguel Oroño <joseomaker@gmail.com>
2024-10-15 01:31:31 -0500

    Configure Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-//import vercel from "@astrojs/vercel/serverless";
-import vercel from "@astrojs/vercel/static";
 
-import mdx from "@astrojs/mdx";
+import react from '@astrojs/react';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
+
+import playformCompress from '@playform/compress';
+
+import mdx from '@astrojs/mdx';
+
+//import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react(), tailwind(), sitemap(), mdx(), playformCompress()],
   site: 'https://exologic.com/', // this line is required
-  integrations: [sitemap(), tailwind(), mdx()],
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: 'server',
+  // output: "static",
+  adapter: cloudflare()
 });
-
-
