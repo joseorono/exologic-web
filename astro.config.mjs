a358d90f6fdbf3c4dc193c22e4399a1b133ad122
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2024-10-30 15:47:51 -0400

    Remove cloudFlare from the proyect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import playformCompress from '@playform/compress';
+import playformCompress from "@playform/compress";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 //import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), sitemap(), mdx(), playformCompress()],
-  site: 'https://exologic.com/', // this line is required
-  output: 'server',
+  site: "https://exologic.com/", // this line is required
+  output: "server",
   // output: "static",
-  adapter: cloudflare()
 });
